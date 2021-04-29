ff723715886c9ec370614a9a0081fce5d152a7fe
Amanda Nolasco <nolasco.amanda@outlook.com>
2021-04-28 21:12:19 -0300

    Criei o projeto no XCODE

---
+++ loteriaApp/loteriaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loteriaApp
+//
+//  Created by Adriel Alves on 28/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
